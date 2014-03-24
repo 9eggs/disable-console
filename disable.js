2328fbfb84b47b3270a4e6cba0862fcf958681e6
Mike Morley <mike.morley@9eggs.com>
2014-03-24 10:38:59 +0000

    Addiing

---
+++ disable.js
@@ -0,0 +1,18 @@
+(function() {
+    try {
+        var $_console$$ = console;
+        Object.defineProperty(window, "console", {
+            get: function() {
+                if ($_console$$._commandLineAPI)
+                    throw "Sorry, for security reasons, the script console is deactivated";
+                return $_console$$
+            },
+            set: function($val$$) {
+                $_console$$ = $val$$
+            }
+        })
+    } catch ($ignore$$) {
+    }
+})();
+
+#http://davidwalsh.name/disable-console
